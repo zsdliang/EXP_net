b486a888a06a60e77458734e11a1a6688283ad83
zsdliang <2726844928@qq.com>
2023-04-05 10:41:01 +0800

    complete eth_in and eth_out functions

--- src/ethernet.c
+++ src/ethernet.c
@@ -1,8 +1,15 @@
 #include "ethernet.h"
+#include "buf.h"
+#include "config.h"
+#include "net.h"
 #include "utils.h"
 #include "driver.h"
 #include "arp.h"
 #include "ip.h"
+#include <stdint.h>
+#include <string.h>
+uint8_t broad_cast_mac[NET_MAC_LEN] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+uint8_t my_mac[NET_MAC_LEN] = NET_IF_MAC; 
 /**
  * @brief 处理一个收到的数据包
  * 
@@ -11,6 +18,21 @@
 void ethernet_in(buf_t *buf)
 {
     // TO-DO
+    if(buf->len < sizeof(ether_hdr_t)) {
+        fprintf(stderr,"error,package length less than earth-net header length");
+        return;
+    }
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    uint16_t protocol16 = swap16(hdr->protocol16);
+    uint8_t dst[NET_MAC_LEN];
+    uint8_t src[NET_MAC_LEN];
+    memcpy(dst,hdr->dst,NET_MAC_LEN);
+    memcpy(src,hdr->src,NET_MAC_LEN);
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+    
+    if(!memcmp(dst,broad_cast_mac,NET_MAC_LEN)||!memcmp(dst,my_mac,NET_MAC_LEN)) {
+        net_in(buf, protocol16, src);
+    }
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -22,6 +44,19 @@ void ethernet_in(buf_t *buf)
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
 {
     // TO-DO
+    //padding if less than 46 byte
+    if(buf->len < 46) {
+        buf_add_padding(buf, 46-buf->len);
+    }
+   
+    //creat earth net header
+    buf_add_header(buf, sizeof(ether_hdr_t));
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    memcpy(hdr->dst, mac,NET_MAC_LEN);
+    memcpy(hdr->src, my_mac,NET_MAC_LEN);
+    hdr->protocol16=swap16(protocol);
+
+    driver_send(buf);
 }
 /**
  * @brief 初始化以太网协议
